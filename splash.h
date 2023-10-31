e323f89f01c1ccfef83abc390631475a209831aa
ehteshamulhassan.232 <ehteshamulhassan.232@northsouth.edu>
2023-11-01 00:46:18 +0600

    Added splash.h

---
+++ splash.h
@@ -0,0 +1,20 @@
+#ifndef SPLASH_H_INCLUDED
+#define SPLASH_H_INCLUDED
+
+void splash_screen(void)    // This function is created to read the ASCII image from Splash.txt and display it
+{
+    FILE *splash = fopen("Splash.txt", "r");
+    if (splash == NULL){
+        printf("Error!\n");
+    return -1;}
+
+    char art[10000];
+    while(!feof(splash))
+    {
+        fgets(art, 10000, splash);
+        printf("%s", art);
+    }
+    fclose(splash);
+}
+
+#endif // SPLASH_H_INCLUDED
