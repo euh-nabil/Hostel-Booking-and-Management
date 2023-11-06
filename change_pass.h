fdd74c7482a2df49d861ffaf32e3d8ffe9a5e16a
ehteshamulhassan.232 <ehteshamulhassan.232@northsouth.edu>
2023-11-07 01:37:59 +0600

    Created change_pass.h
    With this header file, the Admin Password can be changed.

---
+++ change_pass.h
@@ -0,0 +1,40 @@
+#ifndef CHANGE_PASS_H_INCLUDED
+#define CHANGE_PASS_H_INCLUDED
+
+void change_pass(void) {
+
+    printf("\n\n\t\t\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb CHANGE ADMIN PASSWORD"
+           " \xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\n");
+
+    char username[100];
+    char old_password[100];
+    char new_password[100];
+
+    FILE *file = fopen("Admin Password.txt", "r");
+
+    if (fscanf(file, "%s %s", username, old_password) != 2)
+    {
+        printf("Error!\n");
+        fclose(file);
+        return;
+    }
+
+    fclose(file);
+
+    printf("\n\t\tOld Password: %s\n", old_password);
+    printf("\n\t\tEnter the new password: ");
+    scanf("%s", new_password);
+
+    FILE *temp_file = fopen("temp_admin_password.txt", "w");
+
+    fprintf(temp_file, "%s %s", username, new_password);
+
+    fclose(temp_file);
+
+    remove("Admin Password.txt");
+    rename("temp_admin_password.txt", "Admin Password.txt");
+
+    printf("\n\n\t\tPassword changed successfully!\n\n");
+}
+
+#endif // CHANGE_PASS_H_INCLUDED
